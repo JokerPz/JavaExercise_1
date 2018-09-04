fa3480690ea4e2559590f7a3e2f817d0c2a0026e
joker_pz <lpinzhou@163.com>
2018-09-04 11:21:48 +0800

    modify

--- src/com/joker/gitexercise/HelloWorld.java
+++ src/com/joker/gitexercise/HelloWorld.java
@@ -3,5 +3,6 @@ package com.joker.gitexercise;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from Git.");
+		System.out.println("Do some modification.");
 	}
 }
